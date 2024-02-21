123b4e1afdaa4dbad0b6b41135e8a6f4b132e3fb
Eric <bonderic44@gmail.com>
2024-02-22 00:11:51 +0545

    feat : removing active class, setting max-height and opening current tab logic added

---
+++ 29. Accordion/script.js
@@ -0,0 +1,27 @@
+const acc = document.querySelectorAll(".accordion");
+
+for (let i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function () {
+    const desc = this.nextElementSibling;
+    const allDesc = document.querySelectorAll(".desc");
+    const activeAcc = document.getElementsByClassName("accordion active");
+    if (desc.style.maxHeight) {
+      desc.style.maxHeight = null;
+      this.classList.remove("active");
+    } else {
+      // Remove active class from all tabs
+      for (let i = 0; i < activeAcc.length; i++) {
+        activeAcc[i].classList.remove("active");
+      }
+
+      // Set the maxHeight of all desc to 0
+      for (let i = 0; i < allDesc.length; i++) {
+        allDesc[i].style.maxHeight = null;
+      }
+
+      // Open the current tab clicked
+      desc.style.maxHeight = desc.scrollHeight + "px";
+      this.classList.add("active");
+    }
+  });
+}
